16d41f314e2b764316ecf01f985e60e147832065
ruvazi <Runevazi@gmail.com>
2017-09-29 11:55:56 +0200

    something is wierd

--- AssignmentThree/AssignmentThree/operations/Difference.cs
+++ AssignmentThree/AssignmentThree/operations/Difference.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace AssignmentThree.operations
 {
-    class Difference
+    class Difference <T>
     {
-    }
+        public HashSet<T> difference(List<T> a, List<T> b)
+        {
+            HashSet<T> resultSet = new HashSet<T>();
+            
+            foreach( T t in a)
+            {
+                if (!b.Contains(t))
+                {
+                    resultSet.Add(t);
+                }
+            }
+            // Somethhing 
+
+            return resultSet;
+        }
+
+}
 }
