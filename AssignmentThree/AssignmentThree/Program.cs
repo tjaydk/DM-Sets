705ce70380a183dac1807081fb54dfc4c0186dfb
Ebski <Ebski91@gmail.com>
2017-09-29 11:20:44 +0200

    added union method

--- AssignmentThree/AssignmentThree/Program.cs
+++ AssignmentThree/AssignmentThree/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using AssignmentThree.operations;
+using System;
+using System.Collections.Generic;
 
 namespace AssignmentThree
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Union<int> union = new Union<int>();
+
+            List<int> a = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> b = new List<int>() { 2, 3, 4, 5, 6, 7 };
+
+            List<int> c = union.CreateUnion(a, b);
+
+            foreach (int i in c)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
--- AssignmentThree/AssignmentThree/operations/Union.cs
+++ AssignmentThree/AssignmentThree/operations/Union.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AssignmentThree.operations
 {
-    class Union
+    /// <summary>
+    /// Take 2 sets, set a, and set b and make a union
+    /// </summary>
+    public class Union<T>
     {
+        /// <summary>
+        /// Takes two sets and creates a union between them.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public List<T> CreateUnion(List<T> a, List<T> b)
+        {
+            List<T> UnionizedSet = a;
+            if (Double.IsInfinity(a.Count) || Double.IsInfinity(b.Count))
+            {
+                throw new NotImplementedException("Vos estas Loca");
+            }
+
+            HashSet<T> setA = new HashSet<T>(a);
+            HashSet<T> setB = new HashSet<T>(b);
+            
+            foreach (T i in setB)
+            {
+                if (!setA.Contains(i))
+                {
+                    UnionizedSet.Add(i);
+                }
+            }
+
+            return UnionizedSet;
+        }
     }
 }
