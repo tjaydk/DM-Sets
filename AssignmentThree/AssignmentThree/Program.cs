63ec452f6131c7cdcd9759957a40d6b7b02b568e
Ebski <Ebski91@gmail.com>
2017-09-29 11:34:38 +0200

    some formatting

--- AssignmentThree/AssignmentThree/Program.cs
+++ AssignmentThree/AssignmentThree/Program.cs
@@ -8,19 +8,14 @@ namespace AssignmentThree
     {
         static void Main(string[] args)
         {
-
+            Intersection<int> test = new Intersection<int>();
             HashSet<int> a = new HashSet<int>() { 2,3,4,5,6};
             HashSet<int> b = new HashSet<int>() { 2,3,4,7,8};
-           
-            Intersection<int> test = new Intersection<int>();
             HashSet<int> c = test.intersection(a, b);
-
-
+            
            // Union<int> union = new Union<int>();
-
            // List<int> a = new List<int>() { 1, 2, 3, 4, 5, 6 };
            // List<int> b = new List<int>() { 2, 3, 4, 5, 6, 7 };
-
            // List<int> c = union.CreateUnion(a, b);
 
             foreach (int i in c)
