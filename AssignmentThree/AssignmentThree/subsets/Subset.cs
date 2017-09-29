4736d8fdae78035a5200f596a7e0269864a256a7
Dennis Michael <dennis_michael@jubii.dk>
2017-09-29 12:20:02 +0200

    Subsets

---
+++ AssignmentThree/AssignmentThree/subsets/Subset.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssignmentThree.subsets
+{
+    class Subset<T>
+    {
+        public int isSubsetOrEqual(List<T> A, List<T> B)
+        {
+            if (Double.IsInfinity(A.Count) || Double.IsInfinity(B.Count)) return 2;
+            bool equal = true;
+
+            HashSet<T> largest = (A.Count <= B.Count) ? new HashSet<T>(B) : new HashSet<T>(A);
+            List<T> smallest = (largest.Equals(A)) ? B : A;
+
+            for (int j = 0; j < smallest.Count; j++)
+            {
+                if (!largest.Contains(smallest[j]))
+                {
+                    equal = false;
+                }
+            }
+
+            if (smallest.Count == largest.Count && equal) return 0;
+            if (equal && smallest.Equals(A)) return -1;
+            if (equal && smallest.Equals(B)) return 1;
+            return -2;
+        }
+    }
+}
